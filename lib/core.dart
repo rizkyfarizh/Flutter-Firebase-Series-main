bdba0170a4a2f548ce709973ee1a4c072aabd4dc
rizkyfarizh <rizkyfarizh96@gmail.com>
2023-12-12 16:53:00 +0700

    First commit

---
+++ lib/core.dart
@@ -0,0 +1,12 @@
+/*
+We believe, the class name must be unique. 
+If there is a conflicting class name in this file,
+it means you have to rename it to something more unique.
+*/
+export 'package:flutter_firebase/features/app/splash_screen/splash_screen.dart';
+export 'package:flutter_firebase/features/user_auth/firebase_auth_implementation/firebase_auth_services.dart';
+export 'package:flutter_firebase/features/user_auth/presentation/pages/home_page.dart';
+export 'package:flutter_firebase/features/user_auth/presentation/pages/login_page.dart';
+export 'package:flutter_firebase/features/user_auth/presentation/pages/sign_up_page.dart';
+export 'package:flutter_firebase/features/user_auth/presentation/widgets/form_container_widget.dart';
+export 'package:flutter_firebase/global/common/toast.dart';
---
+++ lib/main.dart
@@ -0,0 +1,46 @@
+import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/foundation.dart';
+import 'package:flutter/material.dart';
+// ignore: unused_import
+import 'package:firebase_auth/firebase_auth.dart'; // Import Firebase Auth
+import 'package:flutter_firebase/features/app/splash_screen/splash_screen.dart';
+import 'package:flutter_firebase/features/user_auth/presentation/pages/home_page.dart';
+import 'package:flutter_firebase/features/user_auth/presentation/pages/login_page.dart';
+import 'package:flutter_firebase/features/user_auth/presentation/pages/sign_up_page.dart';
+
+Future main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  if (kIsWeb) {
+    await Firebase.initializeApp(
+      options: FirebaseOptions(
+        apiKey: "AIzaSyBf-7gD33SnDx3T9D7ntY1JGcAxjXrhnaE",
+        appId: "1:657399172326:android:dcc3dcb8fa1e6e49a67f27",
+        messagingSenderId: "657399172326",
+        projectId: "signin-example-a3c39",
+        // Your web Firebase config options
+      ),
+    );
+  } else {
+    await Firebase.initializeApp();
+  }
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Flutter Firebase',
+      routes: {
+        '/': (context) => SplashScreen(
+              // Here, you can decide whether to show the LoginPage or HomePage based on user authentication
+              child: LoginPage(),
+            ),
+        '/login': (context) => LoginPage(),
+        '/signUp': (context) => SignUpPage(),
+        '/home': (context) => HomePage(),
+      },
+    );
+  }
+}
